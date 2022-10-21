2285dd6918d5572af57824b6cfd85623b852c0c6
hadeelsala7 <h.salah.technical@gmail.com>
2022-10-21 17:53:18 +0300

    Update Images.js

---
+++ src/Components/Images/Images.js
@@ -0,0 +1,15 @@
+import FLOWER1 from "../../Images/Flower1.jpg";
+import FLOWER2 from "../../Images/Flower2.jpg";
+import FLOWER3 from "../../Images/Flower3.jpg";
+import FLOWER4 from "../../Images/Flower4.jpg";
+import FLOWER5 from "../../Images/Flower5.jpg";
+import FLOWER6 from "../../Images/Flower6.jpg";
+
+export const cardImages = [
+    { src: FLOWER1, matched: false },
+    { src: FLOWER2, matched: false },
+    { src: FLOWER3, matched: false },
+    { src: FLOWER4, matched: false },
+    { src: FLOWER5, matched: false },
+    { src: FLOWER6, matched: false }
+];
